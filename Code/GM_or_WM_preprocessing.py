c3b819073972a387c93473a282eaa5de06b8ad7d
jonathan-peel <jonathan-peel>
2019-08-28 11:17:05 +0100

    Almost completed preparing datasets.

--- Code/GM_or_WM_preprocessing.py
+++ Code/GM_or_WM_preprocessing.py
@@ -1,7 +1,7 @@
 """This program preprocesses all the 'WM only' and 'GM only' Needle OCT_lan
-   BMP images into a large labelled dataset, where each element is the a 1D
-   vertical slice of the images. This dataset is then saved and used for
-   training.
+   BMP images into a large labelled dataset, where each element is a 1D
+   vertical slice of the images. This dataset is then prepared for training
+   and saved.
 """
 
 from matplotlib.image import imread
@@ -12,11 +12,13 @@ from pathlib import Path
 import numpy as np
 import tensorflow as tf
 
-tf.compat.v1.enable_eager_execution  # lets tensorflow behave like python
+tf.compat.v1.enable_eager_execution()  # lets tensorflow behave like python
 
+'Preprocess images'
 # Define the base directory of all the images and convert it into a
 # pathlib.WindowsPath object. Then collect all the WM image paths into a
-# list of of WindowsPath objects and convert these to strings.
+# list of of WindowsPath objects and convert these to strings. WM slices have
+# an index of 0 and GM slices have an index of 1.
 basedir = 'C:/Users/JayPee/OneDrive - Imperial College London/UROP/Needle \
 OCT_Ian/'
 basepath = Path(basedir)
@@ -25,7 +27,8 @@ GM_paths = list(basepath.glob('GM only/**/*.bmp'))
 WM_paths = [str(path) for path in WM_paths]
 GM_paths = [str(path) for path in GM_paths]
 all_paths = WM_paths + GM_paths
-label_string_to_index = {'WM only': 1, 'GM only': 2}
+label_name_to_index = {'WM only': 0, 'GM only': 1}
+label_names = ['WM only', 'GM only']
 
 WM_NUMBER = len(WM_paths)
 GM_NUMBER = len(GM_paths)
@@ -54,9 +57,9 @@ def make_label_list():
     label_list = []
     for counter in range(TOTAL_NUMBER):
         if counter < WM_NUMBER:
-            label_list.append(label_string_to_index['WM only'])
+            label_list.append(label_name_to_index['WM only'])
         elif counter <= TOTAL_NUMBER:
-            label_list.append(label_string_to_index['GM only'])
+            label_list.append(label_name_to_index['GM only'])
     return label_list
 
 
@@ -66,6 +69,7 @@ def normalise(dataset_element):
     return dataset_element / 255
 
 
+'Make dataset'
 # Make a list of 1D slices (as an nd array) then a list of corresponding
 # labels. Convert these lists into datasets and zip then shuffle them ready
 # for training. Note: slices have to be normalised after dataset is created
@@ -77,3 +81,45 @@ slice_dataset = tf.data.Dataset.from_tensor_slices(all_slices)
 slice_dataset.map(normalise, num_parallel_calls=AUTOTUNE)
 label_dataset = tf.data.Dataset.from_tensor_slices(label_list)
 ds = tf.data.Dataset.zip((slice_dataset, label_dataset))
+
+'Prepare dataset for training'
+# Shuffle, repeat and batch dataset. Also enable prefetch to accelerate
+# training. Then separate dataset into training and validation (and extra
+# verification?)
+BATCH_SIZE = 32  # no reason to choose this number
+ds = ds.shuffle(buffer_size=TOTAL_NUMBER)
+ds = ds.repeat()
+ds = ds.batch(BATCH_SIZE)
+ds = ds.prefetch(buffer_size=AUTOTUNE)
+
+
+def display_slices(num_slices):
+    plt.figure()
+    square_side = np.ceil(np.sqrt(num_slices))
+
+    for counter, (slice, label_index) in enumerate(ds.take(num_slices)):
+        plt.subplot(square_side, square_side, counter+1)
+        plt.plot(np.linspace(0, 600, len(list(slice))), slice, 'b-')  # ?Are all the images 600?
+        plt.xlabel('Frequency (Hz)')  # ?Is it Hz?
+        plt.ylabel('Intensity')
+        plt.title(label_names[label_index])
+
+    plt.show()
+
+
+def get_input():
+    num_slices = input('Dataset of slices is complete. How many slices would\
+you like to view? (Type 0 for none): ')
+    while True:
+        try:
+            num_slices = int(num_slices)
+        except ValueError:
+            num_slices = input('Invalid entry. Please input a positive integer: ')
+        else:
+            return num_slices
+
+
+'Inspect dataset'
+# Give the user the option of viewing x slices from the dataset.
+num_slices = get_input()
+display_slices(num_slices)
