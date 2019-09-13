964d03ac060cecb16a3040c9ebfb1eb0202c22cb
jonathan-peel <jonathan-peel>
2019-09-13 16:24:50 +0100

    Low battery backup.

--- GM_WM_or_both_classification.py
+++ GM_WM_or_both_classification.py
@@ -1,7 +1,8 @@
-"""This program preprocesses all the 'WM only' (white matter) and 'GM only'
-   (grey matter) Needle OCT_lan BMP images into a large labelled dataset, where
-   each element is a 1D vertical slice of the images. A neural network is then
-   trained and tested using this data.
+"""This program preprocesses all the 'WM only' (white matter), 'GM only'
+   (grey matter), 'GM bottom' and 'WM bottom' Needle OCT_lan BMP images into a
+   large labelled dataset, with each image preprocessed into many vertical
+   slices. A neural network is then trained to classify this data as 'GM only',
+   'WM only' or 'GM and WM' and tested.
 """
 
 from matplotlib.image import imread
@@ -15,6 +16,7 @@ from tensorflow.data import Dataset
 from tensorflow import keras
 from tensorflow.keras import layers
 import inspect_dataset
+import time
 
 print('\nFinished importing libraries.')
 
@@ -22,23 +24,30 @@ print('\nFinished importing libraries.')
 # Define the base directory of all the images and convert it into a
 # pathlib.WindowsPath object. Then collect all the WM image paths into a
 # list of of WindowsPath objects and convert these to strings. WM slices have
-# an index of 0 and GM slices have an index of 1.
+# an index of 0, GM slices have an index of 1 and slices with both GM and WM
+# (referred to as 'GMWM') have an index of 2.
 basepath = Path.cwd() / 'Needle OCT_Ian'
 WM_paths = list(basepath.glob('WM only/**/*.bmp'))
 GM_paths = list(basepath.glob('GM only/**/*.bmp'))
+GM_bottom_paths = list(basepath.glob('GM Bottom/**/*.bmp'))
+WM_bottom_paths = list(basepath.glob('WM Bottom/**/*.bmp'))
+GMWM_paths = GM_bottom_paths + WM_bottom_paths
 WM_paths = [str(path) for path in WM_paths]
 GM_paths = [str(path) for path in GM_paths]
-all_paths = WM_paths + GM_paths
-label_names = ['WM only', 'GM only']
+GMWM_paths = [str(path) for path in GMWM_paths]
+all_paths = WM_paths + GM_paths + GMWM_paths
+label_names = ['WM only', 'GM only', 'GM and WM']
 
 WM_IMAGES = len(WM_paths)
 GM_IMAGES = len(GM_paths)
+GMWM_IMAGES = len(GMWM_paths)
 TOTAL_IMAGES = len(all_paths)
-print('\nCollected {} OCT scan image paths consisting of {} Grey Matter \
-and {} White Matter images.'.format(TOTAL_IMAGES, GM_IMAGES, WM_IMAGES))
+print('\nCollected {} OCT scan image paths consisting of {} Grey Matter, {} \
+White Matter and {} Grey and White Matter images.'
+      .format(TOTAL_IMAGES, GM_IMAGES, WM_IMAGES, GMWM_IMAGES))
 
 
-def make_image_array_list():
+def make_image_array_list(all_paths):
     """Load the images into numpy arrays."""
     image_array_list = []
     for counter, image_path in enumerate(all_paths):
@@ -55,10 +64,12 @@ def make_label_list():
     """
     label_list = []
     for counter in range(TOTAL_IMAGES):
-        if counter < WM_IMAGES:
+        if counter <= WM_IMAGES:
             label_list.append(0)  # 0: white matter
-        elif counter <= TOTAL_IMAGES:
+        elif counter <= WM_IMAGES+GM_IMAGES:
             label_list.append(1)  # 1: grey matter
+        elif counter <= TOTAL_IMAGES:
+            label_list.append(2)  # 2: both grey and white matter
     return label_list
 
 
@@ -88,21 +99,21 @@ def image_list_to_slice_list(image_list):
 # each image with its class in a list of tuples. Shuffle this list then
 # separate it into separate training, validation and testing lists. For each
 # list, separate and combine each image into an array of all their constituent
-# slices and generate a corresponding list of labels. Make both of these into
+# slices and generate a corresponding list of labels. Make GMWM of these into
 # datasets then zip them together.
 # Each image contains 767 slices.
-image_array_list = make_image_array_list()
+image_array_list = make_image_array_list(all_paths)
 print('\nCompeted storing {} images as arrays.'.format(TOTAL_IMAGES))
 label_list = make_label_list()
 labelled_image_list = list(zip(image_array_list, label_list))
 random.shuffle(labelled_image_list)
 print('\nCollected and shuffled list of all images with labels.')
 
-TRAIN_IMAGES = int(round(0.7 * TOTAL_IMAGES))  # 864
-VAL_IMAGES = int(round(0.2 * TOTAL_IMAGES))  # 247
-TEST_IMAGES = int(round(0.1 * TOTAL_IMAGES))  # 124
-assert TRAIN_IMAGES + VAL_IMAGES + TEST_IMAGES\
-    == TOTAL_IMAGES, 'Rounding error when splitting dataset.'
+TRAIN_IMAGES = int(round(0.7 * TOTAL_IMAGES))
+VAL_IMAGES = int(round(0.2 * TOTAL_IMAGES))
+TEST_IMAGES = int(round(0.1 * TOTAL_IMAGES))
+# assert TRAIN_IMAGES + VAL_IMAGES + TEST_IMAGES\
+#     == TOTAL_IMAGES, 'Rounding error when splitting dataset.'
 
 train_images = labelled_image_list[:TRAIN_IMAGES]
 val_images = labelled_image_list[TRAIN_IMAGES:TRAIN_IMAGES+VAL_IMAGES]
@@ -144,7 +155,7 @@ print('\nFinished batching and shuffling datasets.')
 # Start simple with just a single dense layer.
 model = keras.models.Sequential([
     layers.Dense(128, activation='relu', batch_input_shape=(None, 410)),
-    layers.Dense(2, activation='softmax')
+    layers.Dense(3, activation='softmax')
 ])
 # Compile with normal optimizer and loss
 model.compile(optimizer='adam', loss='sparse_categorical_crossentropy',
@@ -203,7 +214,7 @@ success_rate = round(results[1], 3)
 print('\nFinal test accuracy: {:.1%}\n'.format(success_rate))
 
 'Generate predictions'
-# 0 is white matter, 1 is grey matter.
+# 0 is white matter, 1 is grey matter, 2 is both.
 rand_int = random.randint(0, TEST_IMAGES-1)
 rand_element = next(iter(test_ds.skip(rand_int).take(1)))
 actual_class = label_names[rand_element[1][0].numpy()]
--- test.py
+++
@@ -1,5 +0,0 @@
-"""Module just for messing around and testing things."""
-
-import tensorflow as tf
-import numpy as np
-print('')
